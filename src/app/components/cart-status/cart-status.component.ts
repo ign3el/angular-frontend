08903e8575c98252783687dec4fe88260990c4b4
ign3el <sahtesham@gmail.com>
2020-05-04 02:24:08 +0400

    123

--- src/app/components/cart-status/cart-status.component.ts
+++ src/app/components/cart-status/cart-status.component.ts
@@ -8,10 +8,22 @@ import { CartService } from 'src/app/services/cart.service';
 })
 export class CartStatusComponent implements OnInit {
 
+  totalPrice:number=0.00;
+  totalQuantity:number=0;
   constructor(private cartService:CartService) { }
 
   ngOnInit(): void {
-    
+    this.updateCartStatus();
+  }
+  updateCartStatus() {
+    this.cartService.totalPrice.subscribe(
+      data=> this.totalPrice=data
+    );
+
+    this.cartService.totalQuantity.subscribe(
+      data=>this.totalQuantity=data
+    );
+
   }
 
 }
